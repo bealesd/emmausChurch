ff902a64c40422106ab08dc29d0876fe50a57eeb
David Beales <david.beales@ukho.gov.uk>
2018-07-27 19:06:31 +0100

    Added ie support. Fixed footer bug. Added new anchor styles. Website fully functioning.

--- Emmaus/Controllers/UiController.cs
+++ Emmaus/Controllers/UiController.cs
@@ -17,11 +17,37 @@ namespace Emmaus.Controllers
             return View("About");
         }
 
+
+        public IActionResult LoadHistoryView()
+        {
+            ViewData["Title"] = "Church History";
+            return View("History");
+        }
+
+        public IActionResult LoadMeetTheTeamView()
+        {
+            ViewData["Title"] = "Meet The Team";
+            return View("MeetTheTeam");
+        }
+
         public IActionResult LoadServicesView()
         {
             ViewData["Title"] = "Services";
 
             return View("Services");
+        }
+        public IActionResult LoadChildServicesView()
+        {
+            ViewData["Title"] = "ChildServices";
+
+            return View("ChildServices");
+        }
+
+        public IActionResult LoadAdultServicesView()
+        {
+            ViewData["Title"] = "AdultServices";
+
+            return View("AdultServices");
         }
 
         public IActionResult LoadWelcomeView()
@@ -54,6 +80,19 @@ namespace Emmaus.Controllers
             ViewData["Title"] = "WiderCommunity";
             return View("WiderCommunity");
         }
+
+        public IActionResult LoadAtHomeView()
+        {
+            ViewData["Title"] = "AtHomeWiderCommunity";
+            return View("WiderCommunity");
+        }
+
+        public IActionResult LoadOverseasView()
+        {
+            ViewData["Title"] = "OverseasWiderCommunity";
+            return View("WiderCommunity");
+        }
+
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
